29e4a0aea45264b7fc1263c06dec7a954371bba0
curioushoainam <hoainamtruongute@gmail.com>
2018-10-19 22:50:23 +0700

    add toggle function

--- app/comps/NoteForm.jsx
+++ app/comps/NoteForm.jsx
@@ -2,19 +2,33 @@ import React from 'react';
 
 export default class NoteForm extends React.Component{
 	constructor(props){
-		super(props);		
+		super(props);	
+		this.state = { isAdding: false }	
 	}
 	handleSubmit(e){
 		e.preventDefault();		
 		this.props.handleAdd(this.refs.txt.value);
 		this.refs.txt.value = '';
+		this.toggle();
 	}
+
+	toggle(){
+		this.state.isAdding = !this.state.isAdding;
+		this.setState(this.state);
+	}
+
 	render(){
+		if(this.state.isAdding) 
+			return(
+				<form onSubmit={this.handleSubmit.bind(this)}>
+					<input type="text" ref="txt" placeholder="Enter your note!" />
+					<button onClick={this.add} >Add</button>
+				</form>
+			);
+
 		return(
-			<form onSubmit={this.handleSubmit.bind(this)}>
-				<input type="text" ref="txt" placeholder="Enter your note!" />
-				<button onClick={this.add} >Add</button>
-			</form>
-		);
+			<button onClick={this.toggle.bind(this)} >+</button>
+		)
+
 	}
 }
