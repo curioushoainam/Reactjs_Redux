cedb2b28c94ce35ef6ee5975d3151b85ad9c25d0
curioushoainam <hoainamtruongute@gmail.com>
2018-10-20 16:39:43 +0700

    implement actions

--- app/redux_demo.jsx
+++ app/redux_demo.jsx
@@ -8,8 +8,14 @@ var defaultState = {
 var reducer = (state = defaultState, action) => {
 	switch(action.type){
 		case 'TOGGLE_IS_ADDING':
-			return {...state, isAdding: !state.isAdding}
-			break;
+			return {...state, isAdding: !state.isAdding}			
+
+		case 'ADD_ITEM':
+			return {...state, arr: [...state.arr, action.item]}
+
+		case 'REMOVE_ITEM':
+			return {...state, arr: state.arr.filter((e,i)=>i != action.index)}
+
 		default:
 			return state;
 	} 	
@@ -20,4 +26,16 @@ console.log(store.getState());
 
 // implement the action in reducer
 store.dispatch({type: 'TOGGLE_IS_ADDING'});
+console.log(store.getState());
+
+store.dispatch({
+	type: 'ADD_ITEM',
+	item: 'LARAVEL'
+});
+console.log(store.getState());
+
+store.dispatch({
+	type: 'REMOVE_ITEM',
+	index: 0
+});
 console.log(store.getState());
